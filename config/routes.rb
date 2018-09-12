476da5aab4284e5a355838b25808ea38c17a94dd
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-09-12 10:34:35 +0300

    Generate 'Users' controller w/ 'new' action:  $ rails generate controller Users new

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
+  get 'users/new'
 end
