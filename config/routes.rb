7b31e293fe3ecbf7b2eb9fe7aa8c4f977173c7f5
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-09-12 10:44:06 +0300

    Update route for the signup page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-
-  get 'users/new'
+  get  '/signup', to: 'users#new'
 end
