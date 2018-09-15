154365a7d38205e6ddf565825845e6edb7893c33
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-09-15 09:20:11 +0300

    Test error messages

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,5 +10,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+
+    assert_select "#error_explanation" do
+      assert_select 'li:nth-child(1)', "Name can't be blank"
+      assert_select 'li:nth-child(2)', "Email is invalid"
+      assert_select 'li:nth-child(3)', "Password confirmation doesn't match Password"
+      assert_select 'li:nth-child(4)', "Password is too short (minimum is 6 characters)"
+    end
   end
 end
