fd1f6bfef2aac22c3f19b56831652de4faf26af9
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-09-18 10:38:46 +0300

    Hit the db the 1-st time but return instance var immediately on subsequent innvocations

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,7 @@ module SessionsHelper
   # Returns the current logged-in user (if any)
   def current_user
     if session[:user_id]
-      User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 end
