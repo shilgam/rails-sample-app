cd3462c66b832c552fadb0022cccda32fd0d9eef
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-09-24 00:32:59 +0300

    Allow users to edit only their own info

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other_user = users(:archer)
   end
 
   test "should get new" do
@@ -23,5 +24,22 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     } }
     assert_redirected_to login_url
     assert_not flash.empty?
+  end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get edit_user_path(@user)
+    assert_redirected_to root_url
+    # assert_select flash[:danger], "You do not have an access to this page."
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch user_path(@user), params: { user: {
+      name: @user.name,
+      email: @user.email
+    } }
+    assert_redirected_to root_url
+    # assert_select flash[:danger], "You do not have an access to this page."
   end
 end
