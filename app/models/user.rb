221a63fdd46b1d8969effab9aff3cda2ae8785a8
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-09-29 10:11:23 +0300

    Generalize the authenticated? method: add arg w/ the name of the digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,12 +19,12 @@ class User < ApplicationRecord
   end
 
   # Returns true if the given token matches the digest
-  def authenticated?(remember_token)
-    digest = self.send("remember_digest")
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
 
     return false if digest.nil?
 
-    BCrypt::Password.new(digest).is_password?(remember_token)
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Forgets a user
