c6f8b699961af3ff6156985bbfa03e1431d23f75
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-10-01 15:05:33 +0300

    Refactoring: move some of the user manipulations into the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,6 +32,17 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  # Activates an account
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   class << self
     # Returns the hash digest of the given string
     def digest(unencrypted_password)
