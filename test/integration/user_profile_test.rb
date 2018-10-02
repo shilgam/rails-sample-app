cfaff4f24b5114adc1aecf801e04b92580e0bae6
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-10-02 08:55:19 +0300

    User Profile: show only for activated users

---
+++ test/integration/user_profile_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UsersProfileTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:non_admin_user)
+    @other_user = users(:other_user)
+  end
+  test "activated user" do
+    log_in_as @user
+    get user_path(@other_user)
+    assert_select ".user_info", text: @other_user.name
+  end
+
+  test "inactivated user" do
+    log_in_as @user
+    @other_user.update_attribute(:activated, false)
+    get user_path(@other_user)
+    assert_redirected_to root_path
+  end
+end
