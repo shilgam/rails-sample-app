cbdd7a576eb81f57a0a88f9546df20980eeaf90b
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-10-03 14:14:07 +0300

    Update password reset email templates

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -8,7 +8,7 @@ class UserMailerTest < ActionMailer::TestCase
 
     assert_equal "Account activation", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["noreply@example.com"], mail.from
     assert_match user.name, mail.body.encoded
     assert_match user.activation_token, mail.body.encoded
     assert_match CGI.escape(user.email), mail.body.encoded
@@ -18,8 +18,11 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:non_admin_user)
     user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
+
     assert_equal "Password reset", mail.subject
     assert_equal [user.email], mail.to
-    assert_equal ["from@example.com"], mail.from
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.reset_token, mail.body.encoded
+    assert_match CGI.escape(user.email), mail.body.encoded
   end
 end
