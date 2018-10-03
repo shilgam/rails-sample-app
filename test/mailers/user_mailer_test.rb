7c05068ce8c1ba13fc8a8f468f849f5eca0c4c41
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-10-03 14:05:26 +0300

    Password reset 'create' action

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -15,10 +15,11 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
   test "password_reset" do
-    mail = UserMailer.password_reset
+    user = users(:non_admin_user)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    assert_equal [user.email], mail.to
     assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
   end
 end
