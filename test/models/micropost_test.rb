5c06258e8a43aa414801024f3983e5b7023a0975
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-10-14 00:12:33 +0300

    Micropost model: Test presencing of a user id
    At the console, instantiate a micropost with no user id and blank content. Is it valid? What are the full error messages?
    
    > @post = Micropost.new(user_id: 1, content: '')
    => #<Micropost id: nil, content: "", user_id: 1, created_at: nil, updated_at: nil>
    >
    > @post.valid?
    => false
    >
    > @post.errors.messages
    => {:content=>["can't be blank"]}

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:non_admin_user)
+    # not idiomatically correct
+    @micropost = Micropost.new(content: "Lorum ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user_id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+
+  test "content should not be too long" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
 end
