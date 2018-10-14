11f879c428e7702bef11e7954583de0a77d02a39
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-10-14 22:22:14 +0300

    Render user's microposts. Part 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts
     redirect_to root_url and return unless @user.activated?
   end
 
