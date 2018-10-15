26043ac746840e740a93a1084aa3c9244336105c
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-10-15 13:01:45 +0300

    Micropost fixtures with user associations

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:non_admin_user)
+  end
+
+  test "profile display" do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.name)
+    assert_select 'h1', @user.name
+  end
 end
