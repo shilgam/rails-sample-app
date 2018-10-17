eda31bdf485db7d680bde197654da6510f06b608
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-10-17 11:41:08 +0300

    Micropost actions must require users to be logged in

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,22 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  # Before filters
+
+  # Confirms a logged-in user
+  def logged_in_user
+    return true if logged_in?
+
+    store_location
+    flash[:danger] = "Please log in."
+    redirect_to login_url
+  end
 end
