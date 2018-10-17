64a598ce923c01d4160dd4f7e3d0396293cd30e5
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-10-17 11:47:39 +0300

    Move shared method to the app controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,17 +6,4 @@ class MicropostsController < ApplicationController
 
   def destroy
   end
-
-  private
-
-  # Before filters
-
-  # Confirms a logged-in user
-  def logged_in_user
-    return true if logged_in?
-
-    store_location
-    flash[:danger] = "Please log in."
-    redirect_to login_url
-  end
 end
