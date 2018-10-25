c0bacdca310d69ad237215b9e635c25d36c77231
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-10-25 23:47:03 +0300

    Add following and followers actions to the Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,11 @@ Rails.application.routes.draw do
   get 'password_resets/new'
   get 'password_resets/edit'
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
