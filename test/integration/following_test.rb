463c20bdf7db7b5331dd0d01b8eb803322c79ace
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2018-10-28 00:01:16 +0300

    Render following and followers: sidebar and stats

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -22,4 +22,48 @@ class FollowingTest < ActionDispatch::IntegrationTest
     get user_path(@user)
     assert_select '#follow_form', count: 0
   end
+
+  test "following/followers page" do
+    @other = users(:other_user)
+    @star = users(:superstar)
+    @other.follow(@star)
+
+    # following page
+    get following_user_path(@other)
+    assert_select 'section.user_info' do
+      assert_select '.gravatar'
+      assert_select 'h3', @other.name
+      assert_select "a[href=?]", user_path(@other), text: "view my profile"
+      assert_select '#microposts_count', "Microposts: #{@other.following.count}"
+    end
+    assert_select 'section.stats' do
+      assert_select '.gravatar'
+      @other.following.each do |user|
+        assert_select 'a[href=?]', user_path(user)
+      end
+    end
+    assert_select '.col-md-8' do
+      assert_select 'h1', "Following"
+      assert_not @other.following.empty?
+      @other.following.each do |user|
+        assert_select "a[href=?]", user_path(user)
+      end
+    end
+
+    # followers page
+    get followers_user_path(@star)
+    assert_select 'section.user_info' do
+      assert_select '.gravatar'
+      assert_select 'h3', @star.name
+      assert_select "a[href=?]", user_path(@star), text: "view my profile"
+      assert_select '#microposts_count', "Microposts: #{@star.following.count}"
+    end
+    assert_select '.col-md-8' do
+      assert_select 'h1', "Followers"
+      assert_not @star.followers.empty?
+      @star.followers.each do |user|
+        assert_select "a[href=?]", user_path(user)
+      end
+    end
+  end
 end
